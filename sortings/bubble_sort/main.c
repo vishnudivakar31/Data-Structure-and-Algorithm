7e6b1d405893e59d1a82f9ef10bb7dc5510e0aa0
Vishnu Divakar <vishnudivakar31@gmail.com>
2019-08-29 15:00:27 -0400

    bubble sort with header files

--- sortings/bubble_sort/main.c
+++ sortings/bubble_sort/main.c
@@ -5,33 +5,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-
-struct Array {
-  int *arr;
-  int size;
-  int length;
-};
-
-struct Array* createArray(int length) {
-  struct Array *newArr = (struct Array *) malloc(sizeof(struct Array));
-  newArr->arr = (int *) malloc(length * sizeof(int));
-  newArr->size = length;
-  newArr->length = 0;
-  return newArr;
-}
-
-void addElem(struct Array *a, int item) {
-  if(a->length < a->size) {
-    a->arr[a->length++] = item;
-  }
-}
-
-void displayArray(struct Array *a) {
-  int i = 0;
-  for(; i < a->length; i++) {
-    printf("%d\n", a->arr[i]);
-  }
-}
+#include "array.h"
 
 void sort(struct Array *a) {
   int i, j, temp;
@@ -45,11 +19,6 @@ void sort(struct Array *a) {
       }
     }
   }
-}
-
-void destoryArray(struct Array *a) {
-  free(a->arr);
-  free(a);
 }
 
 int main() {
